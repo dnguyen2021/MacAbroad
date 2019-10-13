7129bc0e74bcaa5da5cd59f036b05df36c9a9e0c
Duane <dnguyen3@macalester.edu>
2019-10-13 13:11:47 -0500

    trying to get tab2 inputs and storing them

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
+import { Component } from '@angular/core';
 
 @NgModule({
   imports: [
@@ -14,4 +15,20 @@ import { Tab2Page } from './tab2.page';
   ],
   declarations: [Tab2Page]
 })
-export class Tab2PageModule {}
+@Component({
+  selector: 'tab2.page',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['./tab2.page.scss',]
+})
+export class Tab2PageModule {
+  selectedMajor: string = '';
+  selectedLanguage: string = '';
+  selectedCountry: string = '';
+  //event handler for the select element's change event
+  selectChangeHandler (event: any) {
+    //update the ui
+    this.selectedMajor = event.target.value;
+    this.selectedLanguage = event.target.value;
+    this.selectedCountry = event.target.value;
+  }
+}
