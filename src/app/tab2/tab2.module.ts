208e77f5b4b3c44e494bce58054098acc6d3d762
Duane <dnguyen3@macalester.edu>
2019-10-16 08:31:58 -0500

    still working

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -1,10 +1,12 @@
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { Component } from '@angular/core';
+
+name
 
 @NgModule({
   imports: [
@@ -14,61 +16,79 @@ import { Component } from '@angular/core';
     RouterModule.forChild([{ path: '', component: Tab2Page }])
   ],
   declarations: [Tab2Page]
+  
 })
 @Component({
   selector: 'tab2.page',
   templateUrl: 'tab2.page.html',
   styleUrls: ['./tab2.page.scss',]
+  
 })
 
-// export class Major {
-//   name: string;
-// }
 
 export class Tab2PageModule {
-  // majors: Major[] = [
-  //   {name: 'any'},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
-  //   {name: ''},
+  majors = [
+    {"name": "Any"},
+    {"name": "American Studies"},
+    {"name": "Anthropology"},
+    {"name": "Art History"},
+    {"name": "Art"},
+    {"name": "Asian Studies"},
+    {"name": "Biology"},
+    {"name": "Chemistry"},
+    {"name": "Chinese"},
+    {"name": "Classics"},
+    {"name": "Computer Science"},
+    {"name": "Economics"},
+    {"name": "Education Studies"},
+    {"name": "English"},
+    {"name": "Enviromental Studies"},
+    {"name": "French"},
+    {"name": "Geography"},
+    {"name": "Geology"},
+    {"name": "German"},
+    {"name": "History"},
+    {"name": "International Studies"},
+    {"name": "Japanese"},
+    {"name": "Latin American Studies"},
+    {"name": "Linguistics"},
+    {"name": "Math"},
+    {"name": "Media Studies"},
+    {"name": "Music"},
+    {"name": "Neuroscience"},
+    {"name": "Philosophy"},
+    {"name": "Physics"},
+    {"name": "Political Science"},
+    {"name": "Psychology"},
+    {"name": "Religious Studies"},
+    {"name": "Russian"},
+    {"name": "Sociology"},
+    {"name": "Spanish"},
+    {"name": "Theatre and Dance"},
+    {"name": "Women's, Gender and Sexuality Studies"}
+  ];
+  constructor(public navCtrl: NavController){
 
-  // ];
-  selectedMajor: string = '';
-  selectedLanguage: string = '';
-  selectedCountry: string = '';
-  //event handler for the select element's change event
-  selectChangeHandler (event: any) {
-    //update the ui
-    this.selectedMajor = event.target.value;
-    this.selectedLanguage = event.target.value;
-    this.selectedCountry = event.target.value;
   }
-  // public optionsFn(): void {
-  //   console.log(this.name);
+// selectedMajor: string = '';
+// selectedLanguage: string = '';
+// selectedCountry: string = '';
+// //event handler for the select element's change event
+// selectChangeHandler (event: any) {
+//   //update the ui
+//   this.selectedMajor = event.target.value;
+//   this.selectedLanguage = event.target.value;
+//   this.selectedCountry = event.target.value;
+// }
+public majorsFn(): void {
+  console.log(this.Major);
 
-  //   let name = this.name;
-  
-  // }
+  let item = this.Major;
+  this.name = item.name;
 
 }
+}
+
+
+
 
