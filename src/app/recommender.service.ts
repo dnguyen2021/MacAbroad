6ec117e813d0635ced9e4125531dd3c86a2dcbe6
Angel Sylvester <asylvest@macalester.edu>
2019-10-20 19:45:18 -0500

    attempted to include provider

---
+++ src/app/recommender.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class RecommenderService {
+
+  configURL = '/Users/angelsylvester/MacAbroad/MacAbroad/src/assets/recommender.json';   
+
+  private major: string; 
+  private location: string;  
+  private language: string; 
+
+
+  constructor(public http: HttpClient) { 
+    }
+
+  getRecommendations(){
+
+  }
+
+  readJSON(){
+    console.log(this.http.get(this.configURL)); 
+    return this.http.get(this.configURL); 
+  }
+
+
+
+
+}
