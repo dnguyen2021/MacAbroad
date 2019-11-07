d5c0f5f1f3d2d0cf5c5a2a7ffbe753c66dd98989
Duane <dnguyen3@macalester.edu>
2019-11-06 23:20:57 -0600

    fix

--- src/app/tab4/tab4.module.ts
+++ src/app/tab4/tab4.module.ts
@@ -9,10 +9,6 @@ import { Tab4Page } from './tab4.page';
 import { Calendar } from '@ionic-native/calendar/ngx';
 import { NgCalendarModule  } from 'ionic2-calendar';
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 7b0d959b825fbb067c67c0a5412d6329b7bdfc74
 
 const routes: Routes = [
   {
@@ -27,11 +23,7 @@ const routes: Routes = [
     FormsModule,
     IonicModule,
     RouterModule.forChild(routes),
-<<<<<<< HEAD
-
-=======
     NgCalendarModule,
->>>>>>> 7b0d959b825fbb067c67c0a5412d6329b7bdfc74
   ],
   declarations: [Tab4Page]
   
