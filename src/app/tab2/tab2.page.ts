96b64ce6d8c5140f4475e93dff2d820bc17f54c7
Duane <dnguyen3@macalester.edu>
2019-11-20 00:58:20 -0600

    tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,7 +9,34 @@ import { DataService } from '../services/data.service';
 
 })
 
+.controller('MyCtrl', function($scope) {
+  $scope.items2 = [];
+  for (var i=0; i<10; i++) {
+    $scope.items2[i] = {
+      name: i,
+      items: [],
+      show: false
+    };
+    for (var j=0; j<3; j++) {
+      $scope.items2[i].items.push(i + '-' + j);
+    }
+  }
+  
+  /*
+   * if given group is the selected group, deselect it
+   * else, select the given group
+   */
+  $scope.toggleItem = function(item) {
+    item.show = !item.show;
+  };
+  $scope.isItemShown = function(item) {
+    return item.show;
+  };
+  
+});
+
 export class Tab2Page implements OnInit{
+  
   public searchTerm4: string = "";
   public items2: any;
 
