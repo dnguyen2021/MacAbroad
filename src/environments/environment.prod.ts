92e1804ca8fa83214519ef8263588f72cd33aa58
angsylvest <31525250+angsylvest@users.noreply.github.com>
2020-01-06 23:39:24 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCGdu2aY4tHaYfzUu3f3CYuVR25kvlhNaM",
-    authDomain: "macabroad-d7ddd.firebaseapp.com",
-    databaseURL: "https://macabroad-d7ddd.firebaseio.com",
-    projectId: "macabroad-d7ddd",
-    storageBucket: "macabroad-d7ddd.appspot.com",
-    messagingSenderId: "637312659482"
+    apiKey: "API KEY”,
+    authDomain: “AUTHDOMAIN”,
+    databaseURL: “DATABASEURL”,
+    projectId: “PROJECTID”,
+    storageBucket: “STORAGEBUCKET”,
+    messagingSenderId: “MESSENGERSENDERID”
   }
 };
